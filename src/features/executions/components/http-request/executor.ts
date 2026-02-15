af1054649c15a3486cea06bc0b3e702994c1e923
nitin bhatt <nitinbhatt9536@gmail.com>
2026-02-15 21:00:48 +0530

    19:node-variables

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,9 +1,18 @@
 import type {NodeExecutor} from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky,{type Options as KyOptions } from "ky";
+import Handlebars from "handlebars";
+
+Handlebars.registerHelper("json",(context)=>{
+    const jsonString=JSON.stringify(context,null,2);
+    const safeString=new Handlebars.SafeString(jsonString);
+    return safeString;
+});
+
 type HttpRequestData={
-    endpoint?:string,
-    method?:"GET"|"POST"|"PUT"|"DELETE"|"PATCH"
+    variableName:string,
+    endpoint:string,
+    method:"GET"|"POST"|"PUT"|"DELETE"|"PATCH"
     body?:string;
 }
 export const httpRequestExecutor:NodeExecutor<HttpRequestData>=async({
@@ -13,24 +22,38 @@ export const httpRequestExecutor:NodeExecutor<HttpRequestData>=async({
     if(!data.endpoint){
         throw new NonRetriableError("Endpoint is required for HTTP Request node")
     }
+    if(!data.variableName){
+        throw new NonRetriableError("Variable name is required for HTTP Request node")
+    }
+    if(!data.method){
+        throw new NonRetriableError("Method is required for HTTP Request node")
+    }
     const result=await step.run("https-request",async()=>{
-        const endpoint=data.endpoint!
-        const method=data.method || "GET";
+        const endpoint=Handlebars.compile(data.endpoint)(context)
+        const method=data.method;
 
         const options:KyOptions={method};
-        if(["POST","PUT","PATCH"].includes(method) && data.body){
-            options.body=data.body;
+        if(["POST","PUT","PATCH"].includes(method)){
+            const resolved=Handlebars.compile(data.body || "{}")(context)
+            JSON.parse(resolved)
+            options.body=resolved;
+            options.headers={
+                "Content-type":"application/json",
+            }
         }
         const response=await ky(endpoint,options);
         const contentType=response.headers.get("content-type")
         const responseData=contentType?.includes("application/json")?await response.json():await response.text();
-        return{
-            ...context,
-            httpResponse:{
+        const responsePayLoad={
+            httpRequestExecutor:{
                 status:response.status,
                 statusText:response.statusText,
                 data:responseData,
-            }
+            },
+        }
+        return{
+            ...context,
+            [data.variableName!]:responsePayLoad,
         }
     })
     return result;
